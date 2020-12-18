b74d38f61cdf740b8e09744941e7e0437115e1aa
Alberto <thebertsinfo@gmail.com>
2020-12-18 16:55:27 +0100

    Impossibile visualizzare tutti i dischi

--- public/app.js
+++ public/app.js
@@ -95,20 +95,10 @@
 
 $(document).ready(function () {
   var source = $("#cards-template").html();
-  var template = Handlebars.compile(source); //Uso jquery per intercettare il cambio di genere su genres-filter
-
-  $('#genres-filter').change(function () {
-    //svuoto il card-container
-    $('.card-container').empty(); //Recupero il valore value delle option
-
-    var genre_selected = $('#genres-filter').val();
-  });
+  var template = Handlebars.compile(source);
   $.ajax({
     url: '../versione-ajax/dischi.php',
     method: 'GET',
-    data: {
-      genre: genre_selected
-    },
     success: function success(element) {
       for (var i = 0; i < element.length; i++) {
         var context = {
@@ -124,6 +114,35 @@ $(document).ready(function () {
     error: function error() {
       console.log('error');
     }
+  }); //Uso jquery per intercettare il cambio di genere su genres-filter
+
+  $('#genres-filter').change(function () {
+    //svuoto il card-container
+    $('.card-container').empty(); //Recupero il valore value delle option
+
+    var genre_selected = $('#genres-filter').val();
+    $.ajax({
+      url: '../versione-ajax/dischi.php',
+      method: 'GET',
+      data: {
+        genre: genre_selected
+      },
+      success: function success(element) {
+        for (var i = 0; i < element.length; i++) {
+          var context = {
+            poster: element[i].poster,
+            title: element[i].title,
+            author: element[i].author,
+            year: element[i].year
+          };
+          var html = template(context);
+          $('.card-container').append(html);
+        }
+      },
+      error: function error() {
+        console.log('error');
+      }
+    });
   });
 });
 
--- src/app.js
+++ src/app.js
@@ -3,20 +3,9 @@ $(document).ready(function(){
     var source   = $("#cards-template").html();
     var template = Handlebars.compile(source);
 
-    //Uso jquery per intercettare il cambio di genere su genres-filter
-    $('#genres-filter').change(function() {
-        //svuoto il card-container
-        $('.card-container').empty();
-        //Recupero il valore value delle option
-        var genre_selected = $('#genres-filter').val();
-    });
-
     $.ajax({
         url: '../versione-ajax/dischi.php',
         method: 'GET',
-        data: {
-            genre: genre_selected
-        },
         success: function(element) {
 
             for (var i = 0; i < element.length; i++) {
@@ -34,6 +23,39 @@ $(document).ready(function(){
         error: function() {
             console.log('error');
         }
+    });
+
+    //Uso jquery per intercettare il cambio di genere su genres-filter
+    $('#genres-filter').change(function() {
+        //svuoto il card-container
+        $('.card-container').empty();
+        //Recupero il valore value delle option
+        var genre_selected = $('#genres-filter').val();
+
+        $.ajax({
+            url: '../versione-ajax/dischi.php',
+            method: 'GET',
+            data: {
+                genre: genre_selected
+            },
+            success: function(element) {
+
+                for (var i = 0; i < element.length; i++) {
+
+                    var context = {
+                        poster: element[i].poster,
+                        title: element[i].title,
+                        author: element[i].author,
+                        year: element[i].year
+                    };
+                    var html = template(context);
+                    $('.card-container').append(html);
+                }
+            },
+            error: function() {
+                console.log('error');
+            }
+        });
     });
 
 
